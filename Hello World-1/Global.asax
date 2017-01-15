92e0d557d44f553c57ea9c36c89b75bfb508d595
origin <cordovix8@gmail.com>
2017-01-15 20:13:29 +0700

    Add project files.

---
+++ Hello World-1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hello_World_1.MvcApplication" Language="C#" %>
---
+++ Hello World-1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello_World_1.Startup))]
+namespace Hello_World_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
